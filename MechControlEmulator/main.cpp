6bc8d5ecde08c4b17c7e9e24e7030d65f61f139f
Ivan Bystrov <iv.bystrov@gmail.com>
2024-08-19 17:40:58 +0300

    Настроил консоль для вывода кириллицы. Сменил кодировку исходников c Windows-1251 на UTF-8 при помощи преобразования в Notepad++.

--- MechControlEmulator/main.cpp
+++ MechControlEmulator/main.cpp
@@ -1,8 +1,28 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QIODevice>
+#include <QtCore/QLocale>
+
+#include <locale>
+
+QTextStream qout(stdout, QIODevice::WriteOnly);
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    setlocale(LC_ALL, "Russian");
+    qout.setEncoding(QStringConverter::System);
+
+
+    qout << "Cистема управления механизмом приветствует вас! " << Qt::endl;
+//    qout << "Текущее положение: " << Qt::endl;
+    qout << "Команды управления: move x <val> | move y <val> | get position | exit " << Qt::endl;
+    qout << "Введите команду: " << Qt::endl;
+
+
+
+
 
     return a.exec();
 }
